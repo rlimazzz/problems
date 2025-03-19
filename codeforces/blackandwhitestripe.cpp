1907a1382fa031a0c055a3e691fd3a10a247e2dd
rlimazzz <ryangabryel2005@gmail.com>
2025-03-19 16:11:44 -0300

    more problems

--- codeforces/blackandwhitestripe.cpp
+++ codeforces/blackandwhitestripe.cpp
@@ -7,50 +7,16 @@ void solve() {
     string entr;
     cin >> a >> b >> entr;
 
-    int f = -1, l = -1, cnt = INT_MIN, variable = 0;
-    vector<pair<int,int>> entrs;
-    for(int i = 0;i < (int)entr.size();i++) {
-        if(entr[i] == 'W' && f == -1) {
-            f = i;
-            l = i;
-            cnt = max(cnt, variable);
-            variable = 0;
-        }
-        else if(entr[i] == 'W') {
-            l = i;
-            cnt = max(cnt, variable);
-            variable = 0;
-        }
-        else if(entr[i] == 'B' && f != -1) {
-            entrs.push_back({f, l});
-            f = -1;
-        }
-        
-        if(i + 1 == (int)entr.size() && f != -1) {
-            entrs.push_back({f, l});
-        }
-        if(entr[i] == 'B') {
-            variable++;
-        }
-
-
-        cout << f << " !! ";
-    }
-    cout << endl;
-
-    cout << cnt << endl;
-
-    if(cnt == b) {
-        cout << 0 << endl;
-        return;
+    vector<int> v(a + 1, 0);
+    for(int i = 1;i <= a;i++) {
+        v[i] = v[i - 1] + int(entr[i - 1] == 'W');
     }
 
-    int ans = 0;
-    for(int i = 0;i < entrs.size();i++) {
-        if(i == 0) {
-    
-        }   
+    int ans = INT_MAX;
+    for(int i = b;i <= a;i++) {
+        ans = min(ans, v[i] - v[i - b]);
     }
+    cout << ans << endl;
 
 }
 
